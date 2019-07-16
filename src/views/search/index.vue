846249a2091c9234f1575309e637a68584c5a6ec
fall66in <fall66in@163.com>
2019-07-16 17:51:00 +0800

    联想建议-基本功能完成及防抖函数的处理

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -8,17 +8,21 @@
 
         <!-- 联想建议表 -->
         <van-cell-group>
+            <!-- <van-cell title="单元格" icon="search" />
             <van-cell title="单元格" icon="search" />
             <van-cell title="单元格" icon="search" />
             <van-cell title="单元格" icon="search" />
             <van-cell title="单元格" icon="search" />
-            <van-cell title="单元格" icon="search" />
-            <van-cell title="单元格" icon="search" />
-            <van-cell title="单元格" icon="search" />
+            <van-cell title="单元格" icon="search" /> -->
+            <van-cell
+            v-for="item in suggestions"
+            :key="item"
+            :title="item"
+            icon="search" />
         </van-cell-group>
 
         <!-- 历史记录 -->
-        <van-cell-group>
+        <!-- <van-cell-group>
           <van-cell title="历史记录">
             <v-icon
             slot="right-icon"
@@ -26,17 +30,41 @@
             style="line-height: inherit;">
             </v-icon>
           </van-cell>
-        </van-cell-group>
+        </van-cell-group> -->
     </div>
 </template>
 
 <script>
+import { getSuggestion } from '@/api/search'
+import { debounce } from 'lodash'
+
 export default {
   name: 'Search',
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      suggestions: []
     }
+  },
+  watch: {
+    // debounce 接受两个参数
+    // 第一参数：执行你的业务逻辑的参数函数
+    // 第二参数：防抖函数
+    // 当你同一时间用频率过快的时候，只有停下来经过指定的时间才会来调用
+    // 防抖函数：因为你每在搜索框输入一个值，浏览器就会请求一次，所有用防抖函数来规定一个时间来调用
+    searchText: debounce(async function (newVal)  {
+    //   console.log(1)
+      newVal = newVal.trim() // 去除首尾空格
+
+      if (!newVal) {
+        return
+      }
+
+      // 如果数据为空，则什么都不做
+      const data = await getSuggestion(newVal)
+      console.log(data)
+      this.suggestions = data.options
+    }, 500)
   }
 }
 </script>

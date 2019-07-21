881d92717a5030ceaa289ef0ed09972878cf4fc2
fall66in <fall66in@163.com>
2019-07-21 22:38:26 +0800

    处理token过期问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import store from '@/store'
 import JSONbig from 'json-bigint'
+import router from '@/router'
+
+const refreshTokenRequest = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn'
+})
 
 // axios.create用于创建一个axios实例，该实例和axios的功能一模一样
 // 说白了就是克隆一个axios
@@ -44,8 +49,44 @@ request.interceptors.response.use(function (response) {
   // 如果响应结果对象中有data，则直接返回这个data数据
   // 如果响应结果对象中没有data，则不作任何处理，直接原样返回这个数据
   return response.data.data || response.data
-}, function (error) {
+}, async error => {
+  console.dir(error)
   // Do something with response error
+  // 如果状态码是401
+  if (error.response && error.response.status === 401) {
+    // 如果没有refresh——token，则直接跳转登录页
+    const { user } = store.state
+
+    // 如果user都没有，则这就添加登录页
+    if (!user) {
+      return router.push({ name: 'login' })
+    }
+
+    // 如果有user，则请求刷新token
+    // 注意：这个请求不要使用request发送，使用一个没有任何副作用的请求对象去发送
+    try {
+      const res = await refreshTokenRequest({
+        method: 'PUT',
+        url: '/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+
+      // 刷新用户token成功了
+      // 将新的token保存到store中
+      store.commit('setUser', {
+        token: res.data.data.token, // 重新获取的访问 token
+        refresh_token: user.refresh_token // 原来的刷新 token
+      })
+
+      // // 把原来因为 401 失败的那个请求给继续发出去
+      return request(error.config)
+    } catch (err) {
+      // 刷新token失败了
+      return router.push({ name: 'login' })
+    }
+  }
   return Promise.reject(error)
 })
 
